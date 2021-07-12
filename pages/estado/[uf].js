f1d0f766f7de7fdecb036047158069420da3e061
Bruno Martins <38863449+bruno1409@users.noreply.github.com>
2021-07-12 12:57:35 -0300

    Updated navbar

--- pages/estado/[uf].js
+++ pages/estado/[uf].js
@@ -62,53 +62,53 @@ export default function Home({populacao, uf, estado, confirmados, mortes, data, 
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="norte" role="button" data-bs-toggle="dropdown">Norte</a>
                                 <ul className="dropdown-menu">
-                                    <li><Link href="/estado/AC"><a className="dropdown-item" href="#">Acre</a></Link></li>
-                                    <li><Link href="/estado/AP"><a className="dropdown-item" href="#">Amapá</a></Link></li>
-                                    <li><Link href="/estado/AM"><a className="dropdown-item" href="#">Amazonas</a></Link></li>
-                                    <li><Link href="/estado/PA"><a className="dropdown-item" href="#">Pará</a></Link></li>
-                                    <li><Link href="/estado/RO"><a className="dropdown-item" href="#">Rondônia</a></Link></li>
-                                    <li><Link href="/estado/RR"><a className="dropdown-item" href="#">Roraima</a></Link></li>
-                                    <li><Link href="/estado/TO"><a className="dropdown-item" href="#">Tocantins</a></Link></li>
+                                    <li><Link href="/estado/ac"><a className="dropdown-item" href="#">Acre</a></Link></li>
+                                    <li><Link href="/estado/ap"><a className="dropdown-item" href="#">Amapá</a></Link></li>
+                                    <li><Link href="/estado/am"><a className="dropdown-item" href="#">Amazonas</a></Link></li>
+                                    <li><Link href="/estado/pa"><a className="dropdown-item" href="#">Pará</a></Link></li>
+                                    <li><Link href="/estado/ro"><a className="dropdown-item" href="#">Rondônia</a></Link></li>
+                                    <li><Link href="/estado/rr"><a className="dropdown-item" href="#">Roraima</a></Link></li>
+                                    <li><Link href="/estado/to"><a className="dropdown-item" href="#">Tocantins</a></Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="nordeste" role="button" data-bs-toggle="dropdown">Nordeste</a>
                                 <ul className="dropdown-menu">
-                                    <li><Link href="/estado/AL"><a className="dropdown-item" href="#">Alagoas</a></Link></li>
-                                    <li><Link href="/estado/BA"><a className="dropdown-item" href="#">Bahia</a></Link></li>
-                                    <li><Link href="/estado/CE"><a className="dropdown-item" href="#">Ceará</a></Link></li>
-                                    <li><Link href="/estado/MA"><a className="dropdown-item" href="#">Maranhão</a></Link></li>
-                                    <li><Link href="/estado/PB"><a className="dropdown-item" href="#">Paraíba</a></Link></li>
-                                    <li><Link href="/estado/PE"><a className="dropdown-item" href="#">Pernambuco</a></Link></li>
-                                    <li><Link href="/estado/PI"><a className="dropdown-item" href="#">Piauí</a></Link></li>
-                                    <li><Link href="/estado/RN"><a className="dropdown-item" href="#">Rio Grande do Norte</a></Link></li>
-                                    <li><Link href="/estado/SE"><a className="dropdown-item" href="#">Sergipe</a></Link></li>
+                                    <li><Link href="/estado/al"><a className="dropdown-item" href="#">Alagoas</a></Link></li>
+                                    <li><Link href="/estado/ba"><a className="dropdown-item" href="#">Bahia</a></Link></li>
+                                    <li><Link href="/estado/ce"><a className="dropdown-item" href="#">Ceará</a></Link></li>
+                                    <li><Link href="/estado/ma"><a className="dropdown-item" href="#">Maranhão</a></Link></li>
+                                    <li><Link href="/estado/pb"><a className="dropdown-item" href="#">Paraíba</a></Link></li>
+                                    <li><Link href="/estado/pe"><a className="dropdown-item" href="#">Pernambuco</a></Link></li>
+                                    <li><Link href="/estado/pi"><a className="dropdown-item" href="#">Piauí</a></Link></li>
+                                    <li><Link href="/estado/rn"><a className="dropdown-item" href="#">Rio Grande do Norte</a></Link></li>
+                                    <li><Link href="/estado/se"><a className="dropdown-item" href="#">Sergipe</a></Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="centro-oeste" role="button" data-bs-toggle="dropdown">Centro-Oeste</a>
                                 <ul className="dropdown-menu">
-                                    <li><Link href="/estado/DF"><a className="dropdown-item" href="#">Distrito Federal</a></Link></li>
-                                    <li><Link href="/estado/GO"><a className="dropdown-item" href="#">Goiás</a></Link></li>
-                                    <li><Link href="/estado/MT"><a className="dropdown-item" href="#">Mato Grosso</a></Link></li>
-                                    <li><Link href="/estado/MS"><a className="dropdown-item" href="#">Mato Grosso do Sul</a></Link></li>
+                                    <li><Link href="/estado/df"><a className="dropdown-item" href="#">Distrito Federal</a></Link></li>
+                                    <li><Link href="/estado/go"><a className="dropdown-item" href="#">Goiás</a></Link></li>
+                                    <li><Link href="/estado/mt"><a className="dropdown-item" href="#">Mato Grosso</a></Link></li>
+                                    <li><Link href="/estado/ms"><a className="dropdown-item" href="#">Mato Grosso do Sul</a></Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="sudeste" role="button" data-bs-toggle="dropdown">Sudeste</a>
                                 <ul className="dropdown-menu">
-                                    <li><Link href="/estado/ES"><a className="dropdown-item" href="#">Espírito Santo</a></Link></li>
-                                    <li><Link href="/estado/MG"><a className="dropdown-item" href="#">Minas Gerais</a></Link></li>
-                                    <li><Link href="/estado/RJ"><a className="dropdown-item" href="#">Rio de Janeiro</a></Link></li>
-                                    <li><Link href="/estado/SP"><a className="dropdown-item" href="#">São Paulo</a></Link></li>
+                                    <li><Link href="/estado/es"><a className="dropdown-item" href="#">Espírito Santo</a></Link></li>
+                                    <li><Link href="/estado/mg"><a className="dropdown-item" href="#">Minas Gerais</a></Link></li>
+                                    <li><Link href="/estado/rj"><a className="dropdown-item" href="#">Rio de Janeiro</a></Link></li>
+                                    <li><Link href="/estado/sp"><a className="dropdown-item" href="#">São Paulo</a></Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="sul" role="button" data-bs-toggle="dropdown">Sul</a>
                                 <ul className="dropdown-menu">
-                                    <li><Link href="/estado/PR"><a className="dropdown-item" href="#">Paraná</a></Link></li>
-                                    <li><Link href="/estado/RS"><a className="dropdown-item" href="#">Rio Grande do Sul</a></Link></li>
-                                    <li><Link href="/estado/SC"><a className="dropdown-item" href="#">Santa Catarina</a></Link></li>
+                                    <li><Link href="/estado/pr"><a className="dropdown-item" href="#">Paraná</a></Link></li>
+                                    <li><Link href="/estado/rs"><a className="dropdown-item" href="#">Rio Grande do Sul</a></Link></li>
+                                    <li><Link href="/estado/sc"><a className="dropdown-item" href="#">Santa Catarina</a></Link></li>
                                 </ul>
                             </li>
                         </ul>
